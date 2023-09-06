3a50e935c072829157cbb15d19113834e56c0cde
Sushree Suman Rath <115968752+sushreerath@users.noreply.github.com>
2023-09-06 22:32:35 +0530

    Update day3.c

--- day3.c
+++ day3.c
@@ -1,5 +1,5 @@
 1.a)23% profit
-2.c)printer -since everything else is input device while printer is an output device.
+2.c)printer -since everything else are input devices while printer is an output device.
 3.d)inevitable,standards,imperatives
 4.
   #include<stdio.h>
