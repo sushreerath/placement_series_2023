300d8dbdcb2c2ef6227420e178394bd60c680c8c
Sushree Suman Rath <115968752+sushreerath@users.noreply.github.com>
2023-09-06 22:30:46 +0530

    Create day3.c

---
+++ day3.c
@@ -0,0 +1,18 @@
+1.a)23% profit
+2.c)printer -since everything else is input device while printer is an output device.
+3.d)inevitable,standards,imperatives
+4.
+  #include<stdio.h>
+int main(){
+    int i,j;
+    for(int i=0;i<4;i++)
+    {
+        for(j=0;j<8;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+5.d)3,7
