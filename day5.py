584bd7ecbb5a6aef4dbeb8e41749d8c06644761c
Sushree Suman Rath <115968752+sushreerath@users.noreply.github.com>
2023-09-12 16:35:43 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,37 @@
+1.c)52500 cones
+ans:-
+Volume of cylinder =π*(r^2)*h
+r= 5 cm= 50 mm
+h= 7 cm = 70 mm
+Volume of cylinder=π*(50)^2*70
+Volume of cylinder=175,000π mm^3
+
+Volume of cone=(1/3)*π*(r^2)*h
+h=1cm=10 mm
+r=1mm
+Volume of one cone=(10/3)π mm^3
+Number of cones =Volume of cylinder/Volume of one cone
+                = 175,000π mm^3 / (10/3)π mm^3
+Number of cones = 52,500 cones
+
+2.d)statement II is the cause and statement I is the effect.
+
+3.b)financial success without emotional balance can cause havoc in a person's life.
+
+4.
+def generate_fibonacci(n):
+    fibonacci= []
+    a, b = 1, 1
+    while len(fibonacci) < n:
+        fibonacci.append(a)
+        a,b=b,a+b
+    return fibonacci
+n = 13  
+
+fib_series = generate_fibonacci(n)
+for term in fib_series:
+    print(term, end=" ")
+
+5.1)O(n)
+
+6.c)fv
